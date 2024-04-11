81376b5130a1d0a5d210a4e4b08f305eedc5e8d4
Surendra Puri Goswami <surendraiitkgp111@gmail.com>
2024-04-11 14:57:05 +0530

    submit

---
+++ myfirstreactsetup/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+	<h1>Welcome to React!!</h1>
+      <h3>Congratulations on setting up your first React app !!!</h3>
+      <p>
+        We hope you have a fun time exploring React and the various frameworks
+        that work to provide a smooth development experience.
+      </p>    
+</div>
+  );
+}
+
+export default App;
